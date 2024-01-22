1d620f7a34c428ff5ff1c055a7575703ef308127
Darren Jensen <darren.jensen@gmail.com>
2024-01-22 18:44:33 +0700

    added a subtract function and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
-
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
+}
+
+#[wasm_bindgen]
+pub fn subtract(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+
+    #[test]
+    fn test_subtract() {
+        assert_eq!(subtract(5, 2), 3);
+    }
 }
